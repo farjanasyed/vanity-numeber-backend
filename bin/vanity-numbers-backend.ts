ca1f1f00e7628363d079c07476d7154a826ca1a1
syed-farjana <syed.farjana@avantgarde-partner.de>
2024-07-12 01:36:41 +0530

    changed the region

--- bin/vanity-numbers-backend.ts
+++ bin/vanity-numbers-backend.ts
@@ -4,8 +4,8 @@ import * as cdk from 'aws-cdk-lib';
 import { VanityNumbersBackendStack } from '../src/vanity-numbers-backend-stack';
 
 const app = new cdk.App();
-const AWS_REGION = 'eu-central-1';
-const AWS_SANDBOX_ACCOUNT = '747827147613';
+const AWS_REGION = 'us-east-1';
+const AWS_SANDBOX_ACCOUNT = '815348399422';
 
 new VanityNumbersBackendStack(app, 'VanityNumbersBackendStack-prod',{
   env: { region: AWS_REGION, account: AWS_SANDBOX_ACCOUNT },
